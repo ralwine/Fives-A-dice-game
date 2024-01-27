e2576c7b8824ca77982982cffd909bef204a2189
Ryan M Alwine <ryanmitchellalwine@gmail.com>
2024-01-26 21:51:53 -0600

    added parameter for rolling again on a double five

--- dice.ts
+++ dice.ts
@@ -9,11 +9,17 @@ function rollTwoDice(): string {
   const die1 = rollDie();
   const die2 = rollDie();
   //return die1 + die2;
+  const rollSum = die1 + die2;
   console.log(`You rolled: ${die1} and ${die2}`);
   // Main function to run the program
   if (die1 === 6 && die2 === 6) {
+    // double 6 is a bus crash; a bust!
     return "Bus Crash!";
+    // other doubles roll again
   } else if (die1 === die2) {
+    return "Roll again!";
+  } else if (rollSum === 5) {
+    // If the sum of the dice is 5, let the user roll again
     return "Roll again!";
   } else {
     return "TURN OVER!";
