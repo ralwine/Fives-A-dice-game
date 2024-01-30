2231f06b6112bc543d5190c95c8995f2da51dd3b
Ryan M Alwine <ryanmitchellalwine@gmail.com>
2024-01-30 12:27:55 -0600

    created HTML file and visible in browser

---
+++ dice.js
@@ -0,0 +1,39 @@
+// Define a function to roll a single die
+function rollDie() {
+    return Math.floor(Math.random() * 6) + 1;
+}
+// Define a function to roll two dice and return the sum
+function rollTwoDice() {
+    var die1 = rollDie();
+    var die2 = rollDie();
+    var rollSum = die1 + die2;
+    return { die1: die1, die2: die2, rollSum: rollSum };
+}
+// Main function to run the program
+function main() {
+    var resultContainer = document.getElementById('result');
+    var rollButton = document.getElementById('rollButton');
+    if (resultContainer && rollButton) {
+        rollButton.addEventListener('click', function () {
+            var _a = rollTwoDice(), die1 = _a.die1, die2 = _a.die2, rollSum = _a.rollSum;
+            var resultMessage;
+            if (die1 === 6 && die2 === 6) {
+                resultMessage = "Bus Crash!!!!!, TURN OVER!";
+            }
+            else if (die1 === die2) {
+                resultMessage = "Roll again!";
+            }
+            else if (rollSum === 5 || die1 === 5 || die2 === 5) {
+                resultMessage = "Roll again!";
+            }
+            else {
+                resultMessage = "TURN OVER!";
+            }
+            if (resultContainer) {
+                resultContainer.textContent = "You rolled: ".concat(die1, " and ").concat(die2, ". Result: ").concat(resultMessage);
+            }
+        });
+    }
+}
+// Run the main function
+main();
--- dice.ts
+++ dice.ts
@@ -1,34 +1,42 @@
 // Define a function to roll a single die
 function rollDie(): number {
-  // Generate a random number between 1 and 6
-  return Math.floor(Math.random() * 6) + 1;
+    return Math.floor(Math.random() * 6) + 1;
 }
 
 // Define a function to roll two dice and return the sum
-function rollTwoDice(): string {
-  const die1 = rollDie();
-  const die2 = rollDie();
-  //return die1 + die2;
-  const rollSum = die1 + die2;
-  console.log(`You rolled: ${die1} and ${die2}`);
-  // Main function to run the program
-  if (die1 === 6 && die2 === 6) {
-    // double 6 is a bus crash; a bust!
-    return "Bus Crash!!!!!, TURN OVER!";
-  } else if (die1 === die2) {
-    // other doubles roll again
-    return "Roll again!";
-  } else if (rollSum === 5) {
-    // If the sum of the dice is 5, let the user roll again
-    return "Roll again!";
-  }else if (die1 === 5|| die2 === 5){
-    // If either die is a 5, let the use roll again
-    return "Roll again!"
-  } else {
-    return "TURN OVER!";
-  }
+function rollTwoDice(): { die1: number, die2: number, rollSum: number } {
+    const die1 = rollDie();
+    const die2 = rollDie();
+    const rollSum = die1 + die2;
+    return { die1, die2, rollSum };
+}
+
+// Main function to run the program
+function main() {
+    const resultContainer = document.getElementById('result');
+    const rollButton = document.getElementById('rollButton');
+
+    if (resultContainer && rollButton) {
+        rollButton.addEventListener('click', function() {
+            const { die1, die2, rollSum } = rollTwoDice();
+            let resultMessage: string;
+
+            if (die1 === 6 && die2 === 6) {
+                resultMessage = "Bus Crash!!!!!, TURN OVER!";
+            } else if (die1 === die2) {
+                resultMessage = "Roll again!";
+            } else if (rollSum === 5 || die1 === 5 || die2 === 5) {
+                resultMessage = "Roll again!";
+            } else {
+                resultMessage = "TURN OVER!";
+            }
+
+            if (resultContainer) {
+                resultContainer.textContent = `You rolled: ${die1} and ${die2}. Result: ${resultMessage}`;
+            }
+        });
+    }
 }
 
 // Run the main function
-//main();
-console.log(rollTwoDice());
+main();
